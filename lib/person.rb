742363cb238a39055981a276d6936a72395c1c6e
klarissasmith <ksmith_email@yahoo.com>
2020-04-23 14:58:11 -0700

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+    def name=(name_of_person)
+            @name = name_of_person
+    end
+
+    def name
+        @name
+    end
+
+    def job=(the_job)
+        @job = the_job
+    end
+
+    def job
+        @job
+    end
+end
