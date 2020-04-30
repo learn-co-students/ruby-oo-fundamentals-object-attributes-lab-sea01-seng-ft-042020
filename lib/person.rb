9b9084fa56e6e86a6cca7ba0429f6f2c378fca79
jassyfrassy <jsmnramirez25@gmail.com>
2020-04-30 08:26:00 -0700

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+class Person
+
+    attr_reader :name, :job
+    attr_writer :person_name, :name, :job
+
+    def person
+    
+        @person = person
+    
+    end
+
+    def person_name(name)
+
+        @person_name = name
+    
+    end
+
+
+end
